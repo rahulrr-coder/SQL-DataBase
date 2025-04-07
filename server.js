4331cc792beb6d022ed86453333c3a82f24fb724
rahulrr-coder <rahulrr5002@protonmail.com>
2025-04-07 10:59:34 +0530

    Update the server

--- server.js
+++ server.js
@@ -21,12 +21,5 @@ db.connect((err) => {
     }
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello, World! Express + MYSQL');
-});
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
 
